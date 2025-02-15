8d43afc39765ef32ca6002672655a90f953d1a6d
Harizikandar <iskandarharith183@gmail.com>
2025-02-15 13:41:28 +0800

    add new varieties of ships

---
+++ Battleship/Carrier.java
@@ -0,0 +1,5 @@
+public class Carrier extends Ship {
+    public Carrier() {
+        super("Carier",5); // Carrier takes 5 spaces
+    }
+}
--- Battleship/GameController.java
+++ Battleship/GameController.java
@@ -21,17 +21,25 @@ public class GameController {
         List<String> pBattleship = Arrays.asList("A1", "A2", "A3", "A4");
         List<String> pDestroyer = Arrays.asList("C1", "C2", "C3");
         List<String> pSubmarine = Arrays.asList("E1", "E2");
+        List<String> pCarrier = Arrays.asList("G1", "G2", "G3", "G4", "G5");
+        List<String> pCruiser = Arrays.asList("I1", "I2", "I3");
         playerBoard.placeShip(new Battleship(), pBattleship);
         playerBoard.placeShip(new Destroyer(), pDestroyer);
         playerBoard.placeShip(new Submarine(), pSubmarine);
+        playerBoard.placeShip(new Carrier(), pCarrier);
+        playerBoard.placeShip(new Cruiser(), pCruiser);
         
         // For enemy (computer) board:
         List<String> eBattleship = Arrays.asList("J7", "J8", "J9", "J10");
         List<String> eDestroyer = Arrays.asList("F5", "F6", "F7");
         List<String> eSubmarine = Arrays.asList("H3", "H4");
+        List<String> eCarrier = Arrays.asList("D1", "D2", "D3", "D4", "D5");
+        List<String> eCruiser = Arrays.asList("B1", "B2", "B3");
         enemyBoard.placeShip(new Battleship(), eBattleship);
         enemyBoard.placeShip(new Destroyer(), eDestroyer);
         enemyBoard.placeShip(new Submarine(), eSubmarine);
+        enemyBoard.placeShip(new Carrier(), eCarrier);
+        enemyBoard.placeShip(new Cruiser(), eCruiser);
     }
 
     // Process the player's attack on the enemy board.
